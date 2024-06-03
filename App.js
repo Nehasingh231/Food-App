ca6b0427353928fde0c08e25f1c00fe002a5356b
Nehasingh231 <nehakumari14072001@gmail.com>
2024-06-03 17:16:59 +0530

    food

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const Header = () => {
+    return(
+        <div className="header">
+        <div className="logo"> 
+        <img src="" /> 
+        </div>
+        <div className="nav-items">
+        <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+        </ul>
+        </div>
+        </div>
+    );
+};
 
-const heading = React.createElement(
-    "h1",
-    {id:"heading"},
-    "hey neha"
-);
+const AppLayout = () => {
+    return(
+        <div className="app">
+        <Header />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<AppLayout />);
