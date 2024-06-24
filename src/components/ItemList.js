db46663ddfe2d7297f76e0e237e92239d243bea0
Nehasingh231 <nehakumari14072001@gmail.com>
2024-06-25 01:50:58 +0530

    food

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,27 +1,47 @@
+
 import { CDN_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addItem } from "../utils/cartSlice";
 
 const ItemList = ({ items }) => {
-    console.log(items);
+    // console.log(items);
+
+  const dispatch = useDispatch();
+
+    const handleAddItem = () =>{
+      //Dispatch an action
+      dispatch(addItem("pizza"));
+    }
 
   return (
     <div>
       {items.map((item) => (
         <div key={item.card.info.id} className="p-2 m-2 border-b-2 border-gray-200 text-left">
-        <img src={CDN_URL + item.card.info.imageId} className="w-20 float-right"/>  
-        <div className="py-3">
+         
+        <div className="w-9/12">
+        <div className="py-2">
           <span>{item.card.info.name}</span>
           <span> - ₹
           {item.card.info.price ? 
           item.card.info.price / 100 :
           item.card.info.defaultPrice / 100}
         </span>
-          
           </div>
           <p className="text-xs text-gray-600">{item.card.info.description}</p>
         </div>
-  ))}
+        <div className="w-3/12 p-4">
+          <div className="absolute">
+            <button className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg"
+             onClick = {handleAddItem}>
+            Add +
+            </button>
+          </div> 
+            <img src={CDN_URL + item.card.info.imageId} className="w-full float-right" />
+          </div>
+          </div> 
+   ))}
     </div>
-  )
-}
+  );
+};
 
 export default ItemList;
